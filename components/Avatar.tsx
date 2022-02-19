0820522a77ce004278c68f32853da504ae5ae991
Adam Juhasz <adam@paytgthr.com>
2022-02-18 19:16:27 -0800

    smarter avi

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,5 +1,5 @@
-const hashCode = (s: string) =>
-  s.split("").reduce((a, b) => {
+const hashCode = (s?: string) =>
+  (s || "").split("").reduce((a, b) => {
     a = (a << 5) - a + b.charCodeAt(0);
     return a & a;
   }, 0);
