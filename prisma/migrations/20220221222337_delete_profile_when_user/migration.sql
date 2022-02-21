63fdf60ced6d6a4a92e35069c60cb718a08e809f
Adam Juhasz <adam@paytgthr.com>
2022-02-21 14:26:24 -0800

    delete profiles along with users

---
+++ prisma/migrations/20220221222337_delete_profile_when_user/migration.sql
@@ -0,0 +1,34 @@
+-- This is an empty migration.
+
+-- delete a row from public."Users" when the user is deleted
+create or replace function public.handle_deleted_user()
+returns trigger as $$
+begin
+  delete from public."Users" where public."Users".id = old.id;
+  return old;
+end;
+$$ language plpgsql security definer;
+
+-- trigger the function every time a user is deleted
+drop trigger if exists on_auth_user_deleted on auth.users;
+create trigger on_auth_user_deleted
+  after delete on auth.users
+  for each row execute procedure public.handle_deleted_user();
+
+
+-- update a row in public."Users" when the email is updated
+create or replace function public.handle_updated_user()
+returns trigger as $$
+begin
+  update public."Users"
+  set email = new.email
+  where id = new.id;
+  return new;
+end;
+$$ language plpgsql security definer;
+
+-- trigger the function every time a user is updated
+drop trigger if exists on_auth_user_updated on auth.users;
+create trigger on_auth_user_updated
+  after update on auth.users
+  for each row execute procedure public.handle_updated_user();
