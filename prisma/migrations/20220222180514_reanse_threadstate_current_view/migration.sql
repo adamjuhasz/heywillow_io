162d917d875a988695c6a251a1c67810b04b74ef
Adam Juhasz <adam@paytgthr.com>
2022-02-22 10:06:54 -0800

    rename

---
+++ prisma/migrations/20220222180514_reanse_threadstate_current_view/migration.sql
@@ -0,0 +1,13 @@
+-- This is an empty migration.
+DROP VIEW "viewthreadstatecurrent";
+
+CREATE OR REPLACE VIEW "ViewThreadStateCurrent" as
+  SELECT * FROM (
+      SELECT *, 
+      rank() OVER (
+          PARTITION BY "threadId"
+          ORDER BY "createdAt" DESC
+      )
+      FROM "ThreadState"
+  ) rank_filter WHERE RANK = 1
+  ORDER BY "createdAt" DESC;
