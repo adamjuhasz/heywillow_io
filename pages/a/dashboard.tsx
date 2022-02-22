b50ef6b9e3171be032a17510bf4f2cf9ad944a8a
Adam Juhasz <adam@paytgthr.com>
2022-02-21 17:11:15 -0800

    Auto go to single team

--- pages/a/dashboard.tsx
+++ pages/a/dashboard.tsx
@@ -1,12 +1,34 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import AppLayout from "layouts/app";
 import AppHeader from "components/App/HeaderHOC";
 import OnboardingTableTop from "components/Dashboard/Onboarding";
 import AppContainer from "components/App/Container";
+import useGetTeams from "client/getTeams";
+import useGetMyInvites from "client/getMyInvites";
 
 export default function TeamPicker(): JSX.Element {
+  const router = useRouter();
+
+  const { data: teams } = useGetTeams();
+  const { data: invites } = useGetMyInvites();
+
+  const nonAccepted = invites?.filter((i) => i.status !== "accepted") || [];
+  const validTeams = teams?.filter(
+    (t) => nonAccepted.findIndex((invite) => invite.teamId === t.id) === -1
+  );
+
+  useEffect(() => {
+    if (validTeams?.length === 1) {
+      router.push({
+        pathname: "/a/[namespace]/dashboard/",
+        query: { namespace: validTeams[0].Namespace.namespace },
+      });
+    }
+  }, [validTeams, router]);
+
   return (
     <>
       <Head>
