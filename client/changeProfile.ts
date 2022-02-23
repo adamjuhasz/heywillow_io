5f5ac57933c8e23a3ca2aae0b69ec4c63850ac7e
Adam Juhasz <adam@paytgthr.com>
2022-02-23 13:23:31 -0800

    allow updating use profile info

---
+++ client/changeProfile.ts
@@ -0,0 +1,44 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { useSWRConfig } from "swr";
+
+import { useSupabase } from "components/UserContext";
+import { SupabaseProfile } from "types/supabase";
+import { path } from "client/getProfile";
+
+type PartialProfile = Partial<SupabaseProfile> & { id: string };
+
+async function changeProfile(
+  supabase: SupabaseClient,
+  options: PartialProfile
+) {
+  const { data, error } = await supabase
+    .from<SupabaseProfile>("Profile")
+    .update({ ...options })
+    .eq("id", options.id);
+
+  if (error) {
+    console.error("error");
+    throw error;
+  }
+
+  return data;
+}
+
+export default function useChangeProfile() {
+  const supabase = useSupabase();
+  const { mutate } = useSWRConfig();
+
+  const change = async (options: PartialProfile) => {
+    try {
+      if (!supabase) {
+        throw new Error("no supabase connection");
+      }
+      await changeProfile(supabase, options);
+      await mutate(path);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return change;
+}
---
+++ client/getProfile.ts
@@ -0,0 +1,39 @@
+import { useDebugValue } from "react";
+import { SupabaseClient } from "@supabase/supabase-js";
+import useSWR from "swr";
+
+import { SupabaseProfile } from "types/supabase";
+import { useSupabase, useUser } from "components/UserContext";
+
+export const path = "/my/profile";
+
+export async function getProfile(supabase: SupabaseClient, userId: string) {
+  const res = await supabase
+    .from<SupabaseProfile>("Profile")
+    .select(`*`)
+    .eq("id", userId)
+    .single();
+
+  if (res.error !== null) {
+    console.error(res.error);
+    throw res.error;
+  }
+
+  console.log("TeamInvite", res.data);
+  return res.data;
+}
+
+export default function useGetProfile() {
+  const supabase = useSupabase();
+  const { user } = useUser();
+
+  const res = useSWR(
+    () => (supabase && user?.id ? path : null),
+    () => getProfile(supabase as SupabaseClient, user?.id as string),
+    { refreshInterval: 60000 }
+  );
+
+  useDebugValue(res.data);
+
+  return res;
+}
