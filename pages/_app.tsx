a1b69d5845c0d49739c6d96b11bad34d13c96418
Adam Juhasz <adam@akitram.com>
2022-03-15 10:24:57 -0700

    fix posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import type { ReactElement, ReactNode } from "react";
 import type { AppProps } from "next/app";
 import type { NextPage } from "next";
 import Script from "next/script";
+import Head from "next/head";
 
 import PageLoading from "components/PageLoading";
 
@@ -21,21 +22,19 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return getLayout(
     <>
-      {process.env.NODE_ENV === "production" ? (
-        <Script
-          strategy="afterInteractive"
-          id="posthog"
-          dangerouslySetInnerHTML={{
-            // eslint-disable-next-line no-secrets/no-secrets
-            __html: `
+      <Head>
+        {process.env.NODE_ENV === "production" ? (
+          <script>
+            {/* eslint-disable-next-line no-secrets/no-secrets */}
+            {`
         !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
         posthog.init('phc_5i7tJM8Uoz14akX81DF6PXpr2IB1BefrJ7bxPoppS6i',{api_host:'https://app.posthog.com'})
-        `,
-          }}
-        />
-      ) : (
-        <></>
-      )}
+        `}
+          </script>
+        ) : (
+          <></>
+        )}
+      </Head>
       <Component {...pageProps} />
       <PageLoading />
     </>
