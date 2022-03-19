2eaff773bf738056a4d4ccd56390c08c16626027
Adam Juhasz <adam@akitram.com>
2022-03-18 19:00:48 -0700

    add a marker

--- server/postmark/sendPostmarkEmailAsTeam.ts
+++ server/postmark/sendPostmarkEmailAsTeam.ts
@@ -107,6 +107,7 @@ export default async function sendPostmarkEmailAsTeam({
                   To: to,
                   Subject: subject,
                   token: token,
+                  notifyTeamMembers: true,
                 }
               );
               return null;
