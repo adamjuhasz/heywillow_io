febf82e1e8b650ed90f4cd1339544e0b23f4e4b0
Adam Juhasz <adam@akitram.com>
2022-03-21 18:52:28 -0700

    user loading

--- pages/a/[namespace]/settings/team/billing.tsx
+++ pages/a/[namespace]/settings/team/billing.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
@@ -13,12 +13,16 @@ import {
   ReturnBody,
 } from "pages/api/v1/billing/create-portal-link";
 import useGetTeamId from "client/getTeamId";
+import Loading from "components/Loading";
 
 export default function TeamBilling(): JSX.Element {
   const router = useRouter();
   const teamId = useGetTeamId();
+  const [loading, setLoading] = useState(false);
 
   const openStripe = async () => {
+    setLoading(true);
+
     if (teamId === undefined) {
       throw new Error("no team id");
     }
@@ -44,6 +48,7 @@ export default function TeamBilling(): JSX.Element {
       }
 
       default:
+        setLoading(false);
         console.error("Could not generate stripe link");
         throw new Error("Could not generate stripe link");
     }
@@ -62,19 +67,25 @@ export default function TeamBilling(): JSX.Element {
 
         <div className="grow space-y-6">
           <SettingsBox
+            disabled={loading}
             title="Change plan"
             explainer=""
             button={null}
             warning="We use Stripe to manage your subscription"
           >
             <button
+              disabled={loading}
               className="flex w-fit min-w-[80px] items-center justify-center rounded-md border border-transparent bg-zinc-100 py-1.5 px-3 text-sm font-normal text-zinc-900 hover:border-zinc-100 hover:bg-transparent hover:text-zinc-100 disabled:cursor-not-allowed disabled:border-zinc-500 disabled:bg-transparent disabled:font-light disabled:text-zinc-500"
               onClick={async (e) => {
                 e.preventDefault();
                 await openStripe();
               }}
             >
-              Open subscription portal hosted by Stripe
+              {loading ? (
+                <Loading className="h-4 w-4" />
+              ) : (
+                "Open subscription portal"
+              )}
             </button>
           </SettingsBox>
         </div>
