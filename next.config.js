c46c9fb9df545057adc05db87b71ff3e1768fd97
Adam Juhasz <adam@akitram.com>
2022-03-21 19:11:03 -0700

    add plauible

--- next.config.js
+++ next.config.js
@@ -26,6 +26,16 @@ const nextConfig = {
       ],
     },
   ],
+  rewrites: async () => [
+    {
+      source: "/p/script.js",
+      destination: "https://plausible.io/js/script.js",
+    },
+    {
+      source: "/api/event", // Or '/api/event/' if you have `trailingSlash: true` in this config
+      destination: "https://plausible.io/api/event",
+    },
+  ],
 };
 
 module.exports = nextConfig;
---
+++ pages/p/script.js.tsx
@@ -0,0 +1,3 @@
+export default function Script() {
+  return <div>wrong file, redirecting</div>;
+}
