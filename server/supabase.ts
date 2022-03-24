f0a66e2e670ac97da8eb0a6cb7ca8d35a974e4b2
Adam Juhasz <adam@akitram.com>
2022-03-24 16:26:35 -0700

    supabase client

--- server/supabase.ts
+++ server/supabase.ts
@@ -1,6 +1,7 @@
 import { SupabaseClient, createClient } from "@supabase/supabase-js";
 
 export const serviceSupabase: SupabaseClient = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+    "https://ygdlkvsxphwkmneocxtd.supabase.co",
   process.env.SUPABASE_SERVICE_ROLE_KEY || ""
 );
