437fe3b3c36dff36a5364fd5ac61a8c13f0985b4
Adam Juhasz <adam@akitram.com>
2022-03-27 10:04:02 -0700

    static workspace

--- data/Demo/Teams.ts
+++ data/Demo/Teams.ts
@@ -1,6 +1,3 @@
-const teams = [
-  { name: "Stealth AI", Namespace: { namespace: "stealth" } },
-  { name: "Stealth Sales", Namespace: { namespace: "stealth-sales" } },
-];
+const teams = [{ name: "Stealth AI", Namespace: { namespace: "stealth" } }];
 
 export default teams;
